dda209b4bbc3544ab94c86cdba632f9e7e56e4c6
wealth-guardian-chiba <147570097+wealth-guardian-chiba@users.noreply.github.com>
2023-10-11 12:54:46 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, send_file
 import os
 import main_function as mf
+import pandas as pd
+from openpyxl import load_workbook
 
 app = Flask(__name)
 
@@ -8,19 +10,27 @@ app = Flask(__name)
 def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file:
-        file_path = os.path.join('uploads', uploaded_file.filename)
-        uploaded_file.save(file_path)
-        df = pd.read_excel(filepath)
-        df1 = mf.function1(df)
-        df2 = mf.function2(df)
+        if upload_file.filename.endwith('.xlsx'):
+            file_path = os.path.join('uploads', uploaded_file.filename)
+            uploaded_file.save(file_path)
+            df = pd.read_excel(filepath)
+            df1 = mf.function1(df)
+            df2 = mf.function2(df)
 
-        edited_file_path1 = os.path.join('edited', 'edited1_' + uploaded_file.filename)
-        edited_file_path2 = os.path.join('edited', 'edited2_' + uploaded_file.filename)
-        # ここで編集されたファイルを保存
+            edited_file_path = os.path.join('edited', 'edited_' + uploaded_file.filename)
+            df1.to_excel(edited_file_path, index=False, engine='openpyxl', sheet_name="債券リスト")
+            df2.to_excel(edited_file_path, index=False, engine='openpyxl', sheet_name="金利CF表")
+            # ここで編集されたファイルを保存
 
-        return jsonify({'message': 'File edited and saved successfully.', 'edited_file_path': edited_file_path1}), jsonify({'message': 'File edited and saved successfully.', 'edited_file_path': edited_file_path2})
-
+            return jsonify({'message': 'File edited and saved successfully.', 'edited_file_path': edited_file_path})
+        else:
+            return jsonify({'error': 'Invalid file type. Please uploed an Excel file with the .xlsx extension.'})
     return 'File not uploaded.'
 
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    edited_file_path = os.path.join('edited', filename)
+    return send_file(edited_file_path, as_attachment=True)
+    
 if __name__ == '__main__':
     app.run(debug=True)
